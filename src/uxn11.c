c95c615bbc17145f6e47c817006d3defe26feddb
Devine Lu Linvega <aliceffekt@gmail.com>
2024-03-04 09:17:55 -0800

    Fixed issue with debug port

--- src/uxn11.c
+++ src/uxn11.c
@@ -267,7 +267,7 @@ main(int argc, char **argv)
 	int i = 1;
 	char *rom;
 	if(i != argc && argv[i][0] == '-' && argv[i][1] == 'v') {
-		fprintf(stdout, "Uxn11 - Varvara Emulator, 28 Feb 2023.\n");
+		fprintf(stdout, "Uxn11 - Varvara Emulator, 4 Mar 2023.\n");
 		i++;
 	}
 	rom = i == argc ? "boot.rom" : argv[i++];
