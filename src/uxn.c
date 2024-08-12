f4d74dcd489579bf4e2b9df550529b3fd6afaabb
Devine Lu Linvega <aliceffekt@gmail.com>
2024-08-12 07:41:22 -0700

    (uxn) Removed sp* indirection

--- src/uxn.c
+++ src/uxn.c
@@ -11,15 +11,15 @@ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 WITH REGARD TO THIS SOFTWARE.
 */
 
-#define OPC(opc, body) {\
-	case 0x00|opc: {enum{_2=0,_r=0}; s = &uxn.wst, sp = &uxn.wst.ptr; body break;}\
-	case 0x20|opc: {enum{_2=1,_r=0}; s = &uxn.wst, sp = &uxn.wst.ptr; body break;}\
-	case 0x40|opc: {enum{_2=0,_r=1}; s = &uxn.rst, sp = &uxn.rst.ptr; body break;}\
-	case 0x60|opc: {enum{_2=1,_r=1}; s = &uxn.rst, sp = &uxn.rst.ptr; body break;}\
-	case 0x80|opc: {enum{_2=0,_r=0}; s = &uxn.wst, kp = uxn.wst.ptr, sp = &kp; body break;}\
-	case 0xa0|opc: {enum{_2=1,_r=0}; s = &uxn.wst, kp = uxn.wst.ptr, sp = &kp; body break;}\
-	case 0xc0|opc: {enum{_2=0,_r=1}; s = &uxn.rst, kp = uxn.rst.ptr, sp = &kp; body break;}\
-	case 0xe0|opc: {enum{_2=1,_r=1}; s = &uxn.rst, kp = uxn.rst.ptr, sp = &kp; body break;}\
+#define OPC(opc, init, body) {\
+	case 0x00|opc: {enum{_2=0,_r=0}; s = &uxn.wst; init; body; break;}\
+	case 0x20|opc: {enum{_2=1,_r=0}; s = &uxn.wst; init; body; break;}\
+	case 0x40|opc: {enum{_2=0,_r=1}; s = &uxn.rst; init; body; break;}\
+	case 0x60|opc: {enum{_2=1,_r=1}; s = &uxn.rst; init; body; break;}\
+	case 0x80|opc: {enum{_2=0,_r=0}; s = &uxn.wst, kp = uxn.wst.ptr; init; s->ptr = kp; body; break;}\
+	case 0xa0|opc: {enum{_2=1,_r=0}; s = &uxn.wst, kp = uxn.wst.ptr; init; s->ptr = kp; body; break;}\
+	case 0xc0|opc: {enum{_2=0,_r=1}; s = &uxn.rst, kp = uxn.rst.ptr; init; s->ptr = kp; body; break;}\
+	case 0xe0|opc: {enum{_2=1,_r=1}; s = &uxn.rst, kp = uxn.rst.ptr; init; s->ptr = kp; body; break;}\
 }
 
 /* Microcode */
@@ -28,8 +28,8 @@ WITH REGARD TO THIS SOFTWARE.
 #define JMI { pc += uxn.ram[pc++] << 8 | uxn.ram[pc++]; }
 #define JMP(x) { if(_2) pc = (x); else pc += (Sint8)(x); }
 #define POx(o) { if(_2) { PO2(o) } else PO1(o) }
-#define PO1(o) { o = s->dat[--*sp]; }
-#define PO2(o) { o = s->dat[--*sp] | (s->dat[--*sp] << 8); }
+#define PO1(o) { o = s->dat[--s->ptr]; }
+#define PO2(o) { o = s->dat[--s->ptr] | (s->dat[--s->ptr] << 8); }
 #define PUx(y) { if(_2) { PU2(y) } else PU1(y) }
 #define PU1(y) { s->dat[s->ptr++] = (y); }
 #define PU2(y) { tt = (y); PU1(tt >> 8) PU1(tt) }
@@ -43,7 +43,7 @@ int
 uxn_eval(Uint16 pc)
 {
 	int a,b,c;
-	Uint8 t, kp, *sp;
+	Uint8 t, kp;
 	Uint16 tt;
 	Stack *s;
 	if(!pc || uxn.dev[0x0f]) return 0;
@@ -57,37 +57,37 @@ uxn_eval(Uint16 pc)
 		/* LIT */ case 0x80: IMM(wst, uxn.ram[pc++]) break;
 		/* L2r */ case 0xe0: IMM(rst, uxn.ram[pc++])
 		/* LIr */ case 0xc0: IMM(rst, uxn.ram[pc++]) break;
-		/* INC */ OPC(0x01, POx(a) PUx(a + 1))
-		/* POP */ OPC(0x02, POx(a))
-		/* NIP */ OPC(0x03, POx(a) POx(b) PUx(a))
-		/* SWP */ OPC(0x04, POx(a) POx(b) PUx(a) PUx(b))
-		/* ROT */ OPC(0x05, POx(a) POx(b) POx(c) PUx(b) PUx(a) PUx(c))
-		/* DUP */ OPC(0x06, POx(a) PUx(a) PUx(a))
-		/* OVR */ OPC(0x07, POx(a) POx(b) PUx(b) PUx(a) PUx(b))
-		/* EQU */ OPC(0x08, POx(a) POx(b) PU1(b == a))
-		/* NEQ */ OPC(0x09, POx(a) POx(b) PU1(b != a))
-		/* GTH */ OPC(0x0a, POx(a) POx(b) PU1(b > a))
-		/* LTH */ OPC(0x0b, POx(a) POx(b) PU1(b < a))
-		/* JMP */ OPC(0x0c, POx(a) JMP(a))
-		/* JCN */ OPC(0x0d, POx(a) PO1(b) if(b) JMP(a))
-		/* JSR */ OPC(0x0e, POx(a) FLP PU2(pc) JMP(a))
-		/* STH */ OPC(0x0f, POx(a) FLP PUx(a))
-		/* LDZ */ OPC(0x10, PO1(a) PEK(b, a, t) PUx(b))
-		/* STZ */ OPC(0x11, PO1(a) POx(b) POK(a, b, t))
-		/* LDR */ OPC(0x12, PO1(a) PEK(b, pc + (Sint8)a, tt) PUx(b))
-		/* STR */ OPC(0x13, PO1(a) POx(b) POK(pc + (Sint8)a, b, tt))
-		/* LDA */ OPC(0x14, PO2(a) PEK(b, a, tt) PUx(b))
-		/* STA */ OPC(0x15, PO2(a) POx(b) POK(a, b, tt))
-		/* DEI */ OPC(0x16, PO1(a) DEI(a, b) PUx(b))
-		/* DEO */ OPC(0x17, PO1(a) POx(b) DEO(a, b))
-		/* ADD */ OPC(0x18, POx(a) POx(b) PUx(b + a))
-		/* SUB */ OPC(0x19, POx(a) POx(b) PUx(b - a))
-		/* MUL */ OPC(0x1a, POx(a) POx(b) PUx(b * a))
-		/* DIV */ OPC(0x1b, POx(a) POx(b) PUx(a ? b / a : 0))
-		/* AND */ OPC(0x1c, POx(a) POx(b) PUx(b & a))
-		/* ORA */ OPC(0x1d, POx(a) POx(b) PUx(b | a))
-		/* EOR */ OPC(0x1e, POx(a) POx(b) PUx(b ^ a))
-		/* SFT */ OPC(0x1f, PO1(a) POx(b) PUx(b >> (a & 0xf) << (a >> 4)))
+		/* INC */ OPC(0x01, POx(a), PUx(a + 1))
+		/* POP */ OPC(0x02, POx(a), 0)
+		/* NIP */ OPC(0x03, POx(a) POx(b), PUx(a))
+		/* SWP */ OPC(0x04, POx(a) POx(b), PUx(a) PUx(b))
+		/* ROT */ OPC(0x05, POx(a) POx(b) POx(c), PUx(b) PUx(a) PUx(c))
+		/* DUP */ OPC(0x06, POx(a), PUx(a) PUx(a))
+		/* OVR */ OPC(0x07, POx(a) POx(b), PUx(b) PUx(a) PUx(b))
+		/* EQU */ OPC(0x08, POx(a) POx(b), PU1(b == a))
+		/* NEQ */ OPC(0x09, POx(a) POx(b), PU1(b != a))
+		/* GTH */ OPC(0x0a, POx(a) POx(b), PU1(b > a))
+		/* LTH */ OPC(0x0b, POx(a) POx(b), PU1(b < a))
+		/* JMP */ OPC(0x0c, POx(a), JMP(a))
+		/* JCN */ OPC(0x0d, POx(a) PO1(b), if(b) JMP(a))
+		/* JSR */ OPC(0x0e, POx(a), FLP PU2(pc) JMP(a))
+		/* STH */ OPC(0x0f, POx(a), FLP PUx(a))
+		/* LDZ */ OPC(0x10, PO1(a), PEK(b, a, t) PUx(b))
+		/* STZ */ OPC(0x11, PO1(a) POx(b), POK(a, b, t))
+		/* LDR */ OPC(0x12, PO1(a), PEK(b, pc + (Sint8)a, tt) PUx(b))
+		/* STR */ OPC(0x13, PO1(a) POx(b), POK(pc + (Sint8)a, b, tt))
+		/* LDA */ OPC(0x14, PO2(a), PEK(b, a, tt) PUx(b))
+		/* STA */ OPC(0x15, PO2(a) POx(b), POK(a, b, tt))
+		/* DEI */ OPC(0x16, PO1(a), DEI(a, b) PUx(b))
+		/* DEO */ OPC(0x17, PO1(a) POx(b), DEO(a, b))
+		/* ADD */ OPC(0x18, POx(a) POx(b), PUx(b + a))
+		/* SUB */ OPC(0x19, POx(a) POx(b), PUx(b - a))
+		/* MUL */ OPC(0x1a, POx(a) POx(b), PUx(b * a))
+		/* DIV */ OPC(0x1b, POx(a) POx(b), PUx(a ? b / a : 0))
+		/* AND */ OPC(0x1c, POx(a) POx(b), PUx(b & a))
+		/* ORA */ OPC(0x1d, POx(a) POx(b), PUx(b | a))
+		/* EOR */ OPC(0x1e, POx(a) POx(b), PUx(b ^ a))
+		/* SFT */ OPC(0x1f, PO1(a) POx(b), PUx(b >> (a & 0xf) << (a >> 4)))
 		}
 	}
 }
--- src/uxn11.c
+++ src/uxn11.c
@@ -267,7 +267,7 @@ main(int argc, char **argv)
 	int i = 1;
 	char *rom;
 	if(i != argc && argv[i][0] == '-' && argv[i][1] == 'v') {
-		fprintf(stdout, "Uxn11 - Varvara Emulator, 11 Aug 2024.\n");
+		fprintf(stdout, "Uxn11 - Varvara Emulator, 12 Aug 2024.\n");
 		i++;
 	}
 	rom = i == argc ? "boot.rom" : argv[i++];
