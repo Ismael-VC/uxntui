3954e7b387755dfc17074d92379281b3f38c5f43
Devine Lu Linvega <aliceffekt@gmail.com>
2024-08-17 12:10:25 -0700

    (uxn.c)Created macros for each reg

--- src/uxn.c
+++ src/uxn.c
@@ -25,28 +25,39 @@ WITH REGARD TO THIS SOFTWARE.
 /* Microcode */
 
 #define REM if(_r) uxn.rst.ptr -= 1 + _2; else uxn.wst.ptr -= 1 + _2;
-#define JMI pc += uxn.ram[pc++] << 8 | uxn.ram[pc++];
-#define JMP(j) if(_2) pc = (j); else pc += (Sint8)(j);
 #define INC(s) uxn.s.dat[uxn.s.ptr++]
 #define DEC(s) uxn.s.dat[--uxn.s.ptr]
-#define POx(o) if(_2) { PO2(o) } else PO1(o)
-#define PO2(o) if(_r) o = DEC(rst) | (DEC(rst) << 8); else o = DEC(wst) | (DEC(wst) << 8);
-#define PO1(o) if(_r) o = DEC(rst); else o = DEC(wst);
-#define PUx(y) if(_2) { PU2(y) } else PU1(y)
-#define PU2(y) tt = (y); PU1(tt >> 8) PU1(tt)
+#define POP(o) if(_r) o = DEC(rst); else o = DEC(wst);
+#define PUx(y) if(_2) { tt = (y); PU1(tt >> 8) PU1(tt) } else PU1(y)
 #define PU1(y) if(_r) INC(rst) = y; else INC(wst) = y;
 #define PUR(y) if(_r) INC(wst) = y; else INC(rst) = y;
-#define GET(o,p) if(_2) PO1(p) PO1(o)
 #define PUT(y,z) PU1(y) if(_2) PU1(z)
+#define JMI pc += uxn.ram[pc++] << 8 | uxn.ram[pc++];
+#define JMP(j) if(_2) pc = (j); else pc += (Sint8)(j);
 #define DEI(i,o,p) o =emu_dei(i); if(_2) p =emu_dei(i + 1);
 #define DEO(i,y,z) emu_deo(i, y); if(_2) emu_deo(i + 1, z);
 #define PEK(i,o,p,r) o = uxn.ram[i]; if(_2) r = i + 1, p = uxn.ram[r];
 #define POK(i,y,z,r) uxn.ram[i] = y; if(_2) r = i + 1, uxn.ram[r] = z;
 
+/* stack-registers */
+
+#define A if(_2) { A2 } else A1
+#define A1 POP(a)
+#define A2 if(_r) a = DEC(rst) | (DEC(rst) << 8); else a = DEC(wst) | (DEC(wst) << 8);
+#define B if(_2) { B2 } else B1
+#define B1 POP(b)
+#define B2 if(_r) b = DEC(rst) | (DEC(rst) << 8); else b = DEC(wst) | (DEC(wst) << 8);
+
+/* memory-registers */
+
+#define X if(_2) POP(xx) POP(x)
+#define Y if(_2) POP(yy) POP(y)
+#define Z if(_2) POP(zz) POP(z)
+
 int
 uxn_eval(Uint16 pc)
 {
-	int a,b,c,d,e,f,k;
+	int a,b,c,x,xx,y,yy,z,zz,k;
 	Uint8 t;
 	Uint16 tt;
 	if(!pc || uxn.dev[0x0f]) return 0;
@@ -60,37 +71,37 @@ uxn_eval(Uint16 pc)
 		/* LIT */ case 0x80: INC(wst) = uxn.ram[pc++]; break;
 		/* L2r */ case 0xe0: INC(rst) = uxn.ram[pc++];
 		/* LIr */ case 0xc0: INC(rst) = uxn.ram[pc++]; break;
-		/* INC */ OPC(0x01, POx(a),PUx(a + 1))
-		/* POP */ OPC(0x02, REM,0)
-		/* NIP */ OPC(0x03, GET(a,b) REM,PUT(a,b))
-		/* SWP */ OPC(0x04, GET(a,b) GET(c,d),PUT(a,b) PUT(c,d))
-		/* ROT */ OPC(0x05, GET(a,b) GET(c,d) GET(e,f),PUT(c,d) PUT(a,b) PUT(e,f))
-		/* DUP */ OPC(0x06, GET(a,b),PUT(a,b) PUT(a,b))
-		/* OVR */ OPC(0x07, GET(a,b) GET(c,d),PUT(c,d) PUT(a,b) PUT(c,d))
-		/* EQU */ OPC(0x08, POx(a) POx(b),PU1(b == a))
-		/* NEQ */ OPC(0x09, POx(a) POx(b),PU1(b != a))
-		/* GTH */ OPC(0x0a, POx(a) POx(b),PU1(b > a))
-		/* LTH */ OPC(0x0b, POx(a) POx(b),PU1(b < a))
-		/* JMP */ OPC(0x0c, POx(a),JMP(a))
-		/* JCN */ OPC(0x0d, POx(a) PO1(b), if(b) JMP(a))
-		/* JSR */ OPC(0x0e, POx(a),PUR(pc >> 8) PUR(pc) JMP(a))
-		/* STH */ OPC(0x0f, GET(a,b),PUR(a) if(_2) PUR(b))
-		/* LDZ */ OPC(0x10, PO1(a),PEK(a,b,c,t) PUT(b,c))
-		/* STZ */ OPC(0x11, PO1(a) GET(b,c),POK(a,b,c,t))
-		/* LDR */ OPC(0x12, PO1(a),PEK(pc+(Sint8)a,b,c,tt) PUT(b,c))
-		/* STR */ OPC(0x13, PO1(a) GET(b,c),POK(pc+(Sint8)a,b,c,tt))
-		/* LDA */ OPC(0x14, PO2(a),PEK(a,b,c,tt) PUT(b,c))
-		/* STA */ OPC(0x15, PO2(a) GET(b,c),POK(a,b,c,tt))
-		/* DEI */ OPC(0x16, PO1(a),DEI(a,b,c) PUT(b,c))
-		/* DEO */ OPC(0x17, PO1(a) GET(b,c),DEO(a,b,c))
-		/* ADD */ OPC(0x18, POx(a) POx(b),PUx(b + a))
-		/* SUB */ OPC(0x19, POx(a) POx(b),PUx(b - a))
-		/* MUL */ OPC(0x1a, POx(a) POx(b),PUx(b * a))
-		/* DIV */ OPC(0x1b, POx(a) POx(b),PUx(a ? b / a : 0))
-		/* AND */ OPC(0x1c, POx(a) POx(b),PUx(b & a))
-		/* ORA */ OPC(0x1d, POx(a) POx(b),PUx(b | a))
-		/* EOR */ OPC(0x1e, POx(a) POx(b),PUx(b ^ a))
-		/* SFT */ OPC(0x1f, PO1(a) POx(b),PUx(b >> (a & 0xf) << (a >> 4)))
+		/* INC */ OPC(0x01, A, PUx(a + 1))
+		/* POP */ OPC(0x02, REM, 0)
+		/* NIP */ OPC(0x03, X REM, PUT(x,xx))
+		/* SWP */ OPC(0x04, X Y, PUT(x,xx) PUT(y,yy))
+		/* ROT */ OPC(0x05, X Y Z, PUT(y,yy) PUT(x,xx) PUT(z,zz))
+		/* DUP */ OPC(0x06, X, PUT(x,xx) PUT(x,xx))
+		/* OVR */ OPC(0x07, X Y, PUT(y,yy) PUT(x,xx) PUT(y,yy))
+		/* EQU */ OPC(0x08, A B, PU1(b == a))
+		/* NEQ */ OPC(0x09, A B, PU1(b != a))
+		/* GTH */ OPC(0x0a, A B, PU1(b > a))
+		/* LTH */ OPC(0x0b, A B, PU1(b < a))
+		/* JMP */ OPC(0x0c, A, JMP(a))
+		/* JCN */ OPC(0x0d, A B1, if(b) JMP(a))
+		/* JSR */ OPC(0x0e, A, PUR(pc >> 8) PUR(pc) JMP(a))
+		/* STH */ OPC(0x0f, X, PUR(x) if(_2) PUR(xx))
+		/* LDZ */ OPC(0x10, A1, PEK(a,x,xx,t) PUT(x,xx))
+		/* STZ */ OPC(0x11, A1 Y, POK(a,y,yy,t))
+		/* LDR */ OPC(0x12, A1, PEK(pc+(Sint8)a,x,xx,tt) PUT(x,xx))
+		/* STR */ OPC(0x13, A1 Y, POK(pc+(Sint8)a,y,yy,tt))
+		/* LDA */ OPC(0x14, A2, PEK(a,x,xx,tt) PUT(x,xx))
+		/* STA */ OPC(0x15, A2 Y, POK(a,y,yy,tt))
+		/* DEI */ OPC(0x16, A1, DEI(a,x,xx) PUT(x,xx))
+		/* DEO */ OPC(0x17, A1 Y, DEO(a,y,yy))
+		/* ADD */ OPC(0x18, A B, PUx(b + a))
+		/* SUB */ OPC(0x19, A B, PUx(b - a))
+		/* MUL */ OPC(0x1a, A B, PUx(b * a))
+		/* DIV */ OPC(0x1b, A B, PUx(a ? b / a : 0))
+		/* AND */ OPC(0x1c, A B, PUx(b & a))
+		/* ORA */ OPC(0x1d, A B, PUx(b | a))
+		/* EOR */ OPC(0x1e, A B, PUx(b ^ a))
+		/* SFT */ OPC(0x1f, A1 B, PUx(b >> (a & 0xf) << (a >> 4)))
 		}
 	}
 }
